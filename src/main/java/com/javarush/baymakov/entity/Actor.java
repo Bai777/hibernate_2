e21a6ac426606bf5ffc55dba52516097ca93c5f9
Чат бот <bai_777@mail.ru>
2026-05-20 17:55:16 +0300

    Начал расставлять аннотации

--- src/main/java/com/javarush/baymakov/entity/Actor.java
+++ src/main/java/com/javarush/baymakov/entity/Actor.java
@@ -1,4 +1,16 @@
 package com.javarush.baymakov.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = 'actor')
 public class Actor {
+    @Id
+    private Long id;
+
+    @Column
+    private String first_name;
 }
