8526bc45dd071581cd33a44c0e5dd226a1edde80
Чат бот <bai_777@mail.ru>
2026-05-21 17:00:26 +0300

    реализовал класс Inventory, Payment, Rental

--- src/main/java/com/javarush/baymakov/entity/Inventory.java
+++ src/main/java/com/javarush/baymakov/entity/Inventory.java
@@ -1,4 +1,29 @@
 package com.javarush.baymakov.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
 public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inventory_id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "film_id")
+    private Film film;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
--- src/main/java/com/javarush/baymakov/entity/Rental.java
+++ src/main/java/com/javarush/baymakov/entity/Rental.java
@@ -1,4 +1,39 @@
 package com.javarush.baymakov.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rental")
+@Getter
+@Setter
 public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rental_id")
+    private Integer id;
+
+    @Column(name = "rental_date", nullable = false)
+    private LocalDateTime rentalDate;
+
+    @ManyToOne
+    @JoinColumn(name = "inventory_id")
+    private Inventory inventory;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @Column(name = "return_date")
+    private LocalDateTime returnDate;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
