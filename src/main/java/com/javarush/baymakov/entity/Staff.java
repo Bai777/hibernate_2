9f680cc194e361297b40f8833274b8f750cbb764
Чат бот <bai_777@mail.ru>
2026-05-21 16:56:55 +0300

    реализовал класс Staff

--- src/main/java/com/javarush/baymakov/entity/Staff.java
+++ src/main/java/com/javarush/baymakov/entity/Staff.java
@@ -1,4 +1,38 @@
 package com.javarush.baymakov.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "staff")
+@Getter
+@Setter
 public class Staff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "staff_id")
+    private Byte id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    private Boolean active;
+    private String username;
+    private String password;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
