9eadc44257d2b49183e717cdecc5702d4e2a0661
Чат бот <bai_777@mail.ru>
2026-05-21 16:53:19 +0300

    реализовал класс City

--- src/main/java/com/javarush/baymakov/entity/City.java
+++ src/main/java/com/javarush/baymakov/entity/City.java
@@ -1,4 +1,27 @@
 package com.javarush.baymakov.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "city")
+@Getter
+@Setter
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private Short id;
+
+    private String city;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
