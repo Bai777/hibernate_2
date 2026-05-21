3a62d751b070547d55caea900b52a65880a35f88
Чат бот <bai_777@mail.ru>
2026-05-21 16:53:55 +0300

    реализовал класс Country

--- src/main/java/com/javarush/baymakov/entity/Country.java
+++ src/main/java/com/javarush/baymakov/entity/Country.java
@@ -1,4 +1,23 @@
 package com.javarush.baymakov.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "country_id")
+    private Short id;
+
+    private String country;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
