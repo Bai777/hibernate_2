1e49a46df5501b4cb723f7a23a9cf707a75dcdec
Чат бот <bai_777@mail.ru>
2026-05-21 16:55:16 +0300

    реализовал класс Language

--- src/main/java/com/javarush/baymakov/entity/Language.java
+++ src/main/java/com/javarush/baymakov/entity/Language.java
@@ -1,4 +1,23 @@
 package com.javarush.baymakov.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "language")
+@Getter
+@Setter
 public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "language_id")
+    private Byte id;
+
+    private String name;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
