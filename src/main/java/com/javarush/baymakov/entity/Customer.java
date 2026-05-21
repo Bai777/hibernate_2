2676c23ce39ce3fac4505157afd779f8728d675e
Чат бот <bai_777@mail.ru>
2026-05-21 09:54:12 +0300

    реализовал класс Customer

--- src/main/java/com/javarush/baymakov/entity/Customer.java
+++ src/main/java/com/javarush/baymakov/entity/Customer.java
@@ -1,4 +1,45 @@
 package com.javarush.baymakov.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Short id;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Column(name = "active", columnDefinition = "TINYINT")
+    private Boolean active;
+
+    @Column(name = "create_date", nullable = false)
+    private LocalDateTime createDate;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
