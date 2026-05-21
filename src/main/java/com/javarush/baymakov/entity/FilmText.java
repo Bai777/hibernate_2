400ace1610ffb3de5073661578c039bfab6caebf
Чат бот <bai_777@mail.ru>
2026-05-21 09:50:25 +0300

    реализовал класс FilmText

--- src/main/java/com/javarush/baymakov/entity/FilmText.java
+++ src/main/java/com/javarush/baymakov/entity/FilmText.java
@@ -1,4 +1,25 @@
 package com.javarush.baymakov.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "film_text")
+@Getter
+@Setter
 public class FilmText {
+
+    @Id
+    @Column(name = "film_id")
+    private Short id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "film_id")
+    private Film film;
+
+    private String title;
+    private String description;
 }
