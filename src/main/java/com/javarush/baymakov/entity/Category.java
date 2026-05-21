724de236d4dfecbe457020b90b81cb9d955bef95
Чат бот <bai_777@mail.ru>
2026-05-21 16:54:36 +0300

    реализовал класс Category

--- src/main/java/com/javarush/baymakov/entity/Category.java
+++ src/main/java/com/javarush/baymakov/entity/Category.java
@@ -1,4 +1,28 @@
 package com.javarush.baymakov.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Byte id;
+
+    private String name;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Film> films = new HashSet<>();
 }
