f3b95f2ba98d65658e2db785734dc7a920455450
Чат бот <bai_777@mail.ru>
2026-05-21 16:56:04 +0300

    реализовал класс Store

--- src/main/java/com/javarush/baymakov/entity/Store.java
+++ src/main/java/com/javarush/baymakov/entity/Store.java
@@ -1,4 +1,29 @@
 package com.javarush.baymakov.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "store")
+@Getter
+@Setter
 public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Byte id;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_staff_id")
+    private Staff managerStaff;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
